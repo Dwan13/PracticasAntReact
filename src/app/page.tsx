7e6a3b8109b4bab91c4a7dc34b0c0b6a70603607
FelipeBrowser <fveloza@viajemos.com>
2024-05-05 20:47:42 -0500

    Ajustando disenio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import EulerPhiSteps from "app/components/euler/Euler";
 import RSAKeyGenerator from "app/components/euler/keyPair";
 import Snake from "app/components/snake/Snake";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+        
         <RSAKeyGenerator></RSAKeyGenerator>
     </main>
   );
